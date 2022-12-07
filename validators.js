3eaea0f8e71f6ee244ddc83e49c4bf642316bc3f
chenyu <chenyu@reconova.cn>
2022-12-07 18:41:58 +0800

    ✨ feat(0.1.0): 校验规则优化

--- validators.js
+++ validators.js
@@ -1,18 +1,29 @@
-// 暂时只校验IPV4，后续加上IPV6的校验
-export const ipAddress = (val) => {
+export const ipv4Validator = val => {
   if (typeof val != 'string') return false;
   const list = val.split('.');
-  if (list.length == 4) return list.every(e => (e && Number(e) >= 0 && Number(e) <= 255));
+  if (list.length <= 4 && list.length >= 1) return list.every(e => (e && Number(e) >= 0 && Number(e) <= 255));
+  return false;
+}
+console.log(ipv4Validator('4.6')); // 这种是合法的，会在最后一位前面补0，也就是4.0.0.6
+
+export const hostnameValidator = val => {
+  if (typeof val != 'string') return false;
+  const list = val.split('.');
+  if (list.every(e => e.length && !e.match(/[^0-9a-zA-Z\-]/g))) return true;
   return false;
 }
 
-console.log(ipAddress(null));
-console.log(ipAddress(undefined));
-console.log(ipAddress(234));
-console.log(ipAddress(''));
-console.log(ipAddress([]));
-console.log(ipAddress('127.0.0.1'));
-console.log(ipAddress('localhost'));
-console.log(ipAddress('4564.234234.432'));
-console.log(ipAddress('1...0'));
-console.log('4.6'); // 这种是合法的，会在最后一位前面补0，也就是4.0.0.6，这种也需要考虑
+export const portValidator = val => {
+  if (typeof val != 'string') return false;
+  if (val >= 0 && val <= 65535) return true;
+  return false;
+}
+
+export const url = val => {
+  // 如果是async-validator, 里面自带有type: 'url'的校验
+  if (typeof val != 'string') return false;
+  const [protocol, host] = val.split('://');
+  const [hostname, port] = host.split(':');
+  if (['http', 'https'].includes(protocol) && (hostnameValidator(hostname) || ipv4Validator(hostname)) && portValidator(port)) return true;
+  return false;
+}
